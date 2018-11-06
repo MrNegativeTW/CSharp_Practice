667724089d3c46c0ed579f0fd92908a2098b6904
TrevorWu <wowpapa3232@gmail.com>
2018-11-06 18:00:50 +0800

    HW2_Done

---
+++ HW2/HW2-3/HW2-3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        int a = 134000, b = 114850, c = 105980;
+
+        private void button1_Click(object sender, EventArgs e) {
+            int total=0;
+            if (checkBox1.Checked) {
+                total += a;
+            }
+            if (checkBox2.Checked) {
+                total += b;
+            }
+            if (checkBox3.Checked) {
+                total += c;
+            }
+            if (total >= 134001) {
+                label2.Text = "金額為:" + Convert.ToString(total) + "，請選贈品";
+                groupBox2.Enabled = true;
+                button2.Enabled = true;
+            }else {
+                label2.Text = "金額為:" + Convert.ToString(total) + "，您太窮了，贈品配不上您";
+                groupBox2.Enabled = false;
+                button2.Enabled = false;
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (radioButton1.Checked) {
+                label3.Text = "AF-S 70-200mm f/2.8G ED VR II";
+            }
+            if (radioButton2.Checked)
+            {
+                label3.Text = "AF-S NIKKOR 50mm f/1.8G";
+            }
+        }
+
+        
+
+
+        private void Form1_Load(object sender, EventArgs e) {
+            
+        }
+
+        
+
+    }
+}
---
+++ HW2/HW2-5/HW2-5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            label2.Left -= 1;
+            if (label2.Left <= 0) {
+                timer2.Enabled = true;
+            } else if (label2.Left <= -99) {
+                label2.Left = 522;
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) {
+            label1.Left -= 1;
+            if (label1.Left <= 0) {
+                timer1.Enabled = true;
+            } else if (label1.Left <= -99) {
+                label1.Left = 522;
+                timer2.Enabled = false;
+            }
+        }
+    }
+}
