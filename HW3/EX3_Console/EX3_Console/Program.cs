1c3fdcf9543f119c6236bcab4ccb50b1a9ccade7
TrevorWu <wowpapa3232@gmail.com>
2018-11-23 10:16:25 +0800

    HW3

---
+++ HW3/EX3_Console/EX3_Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3_Console
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            // 1-1
+            int[,] ss = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            Console.Write(ss.GetUpperBound(1) + "\n");
+
+            //2-1
+            string nameList = "";
+            String[] s_name = new string[] {"String1", "String2"};
+            foreach (String s in s_name) {
+                nameList += s + ",";
+            }
+
+            Console.WriteLine(nameList);
+
+
+            Console.ReadKey();
+        }
+    }
+}
